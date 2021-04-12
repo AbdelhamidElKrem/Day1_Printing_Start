552fa448b7e5aa8e13a3d69ee0c2be7409d20d73
ABDELHAMID EL KREM <>
2021-04-12 17:20:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#la premier phrase
+print("Welcome to the Brand Name Generator");
+
+#ask for the name of city
+city = input ("What's name of the city you grew up in? \n");
+
+#ask for the name ou pet
+pet_name = input("what's your pet's'name \n");
+
+#afficher la resultat;
+print("your band name could be "+ city + pet_name );
